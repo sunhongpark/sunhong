8620acc587c4d379476bf4e11c6f594976e81da2
mmu <mmu@PC-14>
2014-03-18 10:41:54 +0900

    퀴즈

---
+++ src/lab/quiz1/quiz14.java
@@ -0,0 +1,11 @@
+package lab.quiz1;
+public class quiz14 {
+	public static void main(String[] args)
+	{
+		int x,y;
+		x= Integer.parseInt(args[0]);
+		y= Integer.parseInt(args[1]);
+		System.out.println(Math.sqrt(x*x+y*y));
+	}
+
+}
---
+++ src/lab/quiz1/quiz15.java
@@ -0,0 +1,11 @@
+package lab.quiz1;
+
+public class quiz15 {
+	public static void main(String[] args)
+	{
+		System.out.println("17.0/0.0 = "+(17.0/0.0));
+		System.out.println("17.0%0.0 = "+(17.0%0.0));
+		System.out.println("17/0 = "+(17/0));
+		System.out.println("17%0 = "+(17%0));
+	}
+}
