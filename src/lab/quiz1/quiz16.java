87b6dc5323698efc83c2e4c2110e41b16f3be86f
mmu <mmu@PC-14>
2014-04-15 09:07:02 +0900

    a

--- src/lab/quiz1/quiz16.java
+++ src/lab/quiz1/quiz16.java
@@ -1,20 +1,21 @@
 package lab.quiz1;
 
 public class quiz16 {
-	public static void Re(int i)
+	public static int Re(int i)
 	{
 		if(i==0)
 		{
-			System.out.println("a");
-			return;
+			System.out.print("a");
+			return 0;
 		}
 		if(i==1)
-			System.out.println("b");
+			System.out.print("b");
 		else
 		{
 		 Re(i-1); 
 		 Re(i-2);
 		}
+		return Re(i-1);
 	}
 	public static void main(String[] args)
 	{
